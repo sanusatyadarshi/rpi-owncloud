a613fbbe84c949d6cfa9530ac41ca4cb14969aa1
Sanu Satyadarshi <sanusatyadarshi@gmail.com>
2018-03-11 03:33:21 +0530

    replaced localhost with 127.0.0.1

--- resources/cfgmysql.sh
+++ resources/cfgmysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 /usr/bin/mysqld_safe &
 sleep 20
-/usr/bin/mysql -u root -proot123 -e "CREATE DATABASE owncloud; GRANT ALL ON owncloud.* TO 'owncloud'@'localhost' IDENTIFIED BY 'owncloudsql';"
-/usr/bin/mysql -u root -proot123 -e "SET PASSWORD FOR 'owncloud'@'localhost' = PASSWORD('owncloud');"
+/usr/bin/mysql -u root -proot123 -e "CREATE DATABASE owncloud; GRANT ALL ON owncloud.* TO 'owncloud'@'127.0.0.1' IDENTIFIED BY 'owncloudsql';"
+/usr/bin/mysql -u root -proot123 -e "SET PASSWORD FOR 'owncloud'@'127.0.0.1' = PASSWORD('owncloud');"
 pkill -f mysqld
